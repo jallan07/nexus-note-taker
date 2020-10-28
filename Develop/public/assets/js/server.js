02ffb39536805b856a443140113cf4bdc5101964
Josh Allan <allan.josh07@gmail.com>
2020-10-28 10:43:36 -0400

    file upload

---
+++ Develop/public/assets/js/server.js
@@ -0,0 +1,16 @@
+// dependencies & requirements
+const express = require("express");
+const app = express();
+const path = require("path");
+
+// set the port variable
+const PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// route for homepage
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "index.html"));
+});
