89ad046f35cf023fa18f876c596504e5b2caa9d4
Josh Allan <allan.josh07@gmail.com>
2020-10-29 10:18:35 -0400

    Added the middleware code to the server file that serves all public files so that the css and jquery files work properly.

--- server.js
+++ server.js
@@ -11,8 +11,9 @@ const PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + "/public"));
 
-// Main route for the homepage
+// GET  `/` - Should return the homepage
 app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "/public/index.html"));
 });
@@ -35,6 +36,7 @@ app.get("/api/notes", (req, res) => {
 
 // POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
 app.post("/api/notes", (req, res) => {
+	// create the new note based off the user input
 	let newNote = {
 		title: req.body.title,
 		text: req.body.text,
@@ -53,6 +55,9 @@ app.post("/api/notes", (req, res) => {
 	res.send({ msg: "Note saved to the database!" });
 });
 
+// DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+
+// Listen for the server on our PORT
 app.listen(PORT, () =>
 	console.log(`Server listening at http://localhost:${PORT}`)
 );
